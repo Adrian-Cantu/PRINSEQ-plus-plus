7d520a2bec66aec19b1684e1f890a366705d2f5f
Jeffrey Sadural <heffe227@gmail.com>
2014-05-28 17:31:24 -0700

    Initial Commit

---
+++ PrinSeq/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  PrinSeq
+//
+//  Created by Jeffrey Sadural on 5/28/14.
+//  Copyright (c) 2014 Jeffrey Sadural. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
