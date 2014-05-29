e762423cb29d81adc862c9a43f6b3cc90999aea0
Jeffrey Sadural <heffe227@gmail.com>
2014-05-29 11:01:47 -0700

    Committing main

--- PrinSeq/main.cpp
+++ PrinSeq/main.cpp
@@ -1,18 +1,27 @@
-//
-//  main.cpp
-//  PrinSeq
-//
-//  Created by Jeffrey Sadural on 5/28/14.
-//  Copyright (c) 2014 Jeffrey Sadural. All rights reserved.
-//
+// Copyright Vladimir Prus 2002-2004.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt
+// or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+/* The simplest usage of the library.
+ */
+
+#include <boost/program_options.hpp>
+#include "Fasta.h"
+namespace po = boost::program_options;
 
 #include <iostream>
+#include <iterator>
+using namespace std;
 
-int main(int argc, const char * argv[])
+int main(int argc, char* argv[])
 {
-
-    // insert code here...
-    std::cout << "Hello, World!\n";
+	int optind = 1;
+    Fasta FA(argc, argv);
+//	if (strcmp(argv[optind],"-fasta") == 0)
+//	{
+//		Fasta FA(argc, argv);
+//	}
+    
     return 0;
 }
-
