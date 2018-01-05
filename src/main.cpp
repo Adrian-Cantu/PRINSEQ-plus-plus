b6d9e1e2ec1b36711c27407ebaf909c10f99f9e8
Adrian Cantu <garbanyo@gmail.com>
2018-01-05 22:48:29 +0000

    added support for single end, need to add many functions to single end loop

--- src/main.cpp
+++ src/main.cpp
@@ -183,11 +183,13 @@ int main (int argc, char **argv)
         cerr << "Error: can not open " << forward_read_file  << endl ;
         return 1;
     }
-    inFile_r.open(reverse_read_file);
-    if (!inFile_r) {
-        cerr << "Error: can not open " << reverse_read_file  << endl ;
-        return 1;
-    }
+    if (reverse_read_file) {
+        inFile_r.open(reverse_read_file);
+        if (!inFile_r) {
+            cerr << "Error: can not open " << reverse_read_file  << endl ;
+            return 1;
+        }
+    }    
       
 ////////// open and name oupput files
     ofstream bad_out_file_R1;
@@ -198,24 +200,31 @@ int main (int argc, char **argv)
     ofstream good_out_file_R2;
 
     if (out_format == 1 ) { out_ext = "fasta";}
-    bad_out_file_R1.open("Test_" + out_name  + "_bad_out_R1." + out_ext );
-    single_out_file_R1.open("Test_" + out_name  + "_single_out_R1." + out_ext  );
-    good_out_file_R1.open("Test_" + out_name  + "_good_out_R1." + out_ext);
-    bad_out_file_R2.open("Test_" + out_name  + "_bad_out_R2." + out_ext );
-    single_out_file_R2.open("Test_" + out_name  + "_single_out_R2." + out_ext);
-    good_out_file_R2.open("Test_"+ out_name + "_good_out_R2."  + out_ext);
-
+    
+    
+    if (reverse_read_file) {
+        bad_out_file_R1.open("Test_" + out_name  + "_bad_out_R1." + out_ext );
+        single_out_file_R1.open("Test_" + out_name  + "_single_out_R1." + out_ext  );
+        good_out_file_R1.open("Test_" + out_name  + "_good_out_R1." + out_ext);
+        bad_out_file_R2.open("Test_" + out_name  + "_bad_out_R2." + out_ext );
+        single_out_file_R2.open("Test_" + out_name  + "_single_out_R2." + out_ext);
+        good_out_file_R2.open("Test_"+ out_name + "_good_out_R2."  + out_ext);
+    } else {
+        bad_out_file_R1.open("Test_" + out_name  + "_bad_out." + out_ext );
+        single_out_file_R1.open("Test_" + out_name  + "_single_out." + out_ext  );
+        good_out_file_R1.open("Test_" + out_name  + "_good_out." + out_ext);
+    }
 
     single_read read_f(inFile_f);
     single_read read_r(inFile_r);
-
     pair_read read_rf(inFile_f,inFile_r);
-
-    read_rf.set_outputs(bad_out_file_R1,single_out_file_R1,good_out_file_R1,
-        bad_out_file_R2,single_out_file_R2,good_out_file_R2);
-    
-    read_rf.set_out_format(out_format);
-    
+    if (reverse_read_file) {
+        read_rf.set_outputs(bad_out_file_R1,single_out_file_R1,good_out_file_R1,
+            bad_out_file_R2,single_out_file_R2,good_out_file_R2);
+        read_rf.set_out_format(out_format);
+    } else {
+        read_f.set_outputs(bad_out_file_R1,single_out_file_R1,good_out_file_R1);
+    }
     bloom_parameters parameters;
     bloom_filter *filter=NULL;
     if (derep) {    
@@ -229,38 +238,46 @@ int main (int argc, char **argv)
         parameters.compute_optimal_parameters();
         filter  = new bloom_filter(parameters);
     }
-
+    
     // main loop
-    while(read_rf.read_read()) {
+    if (reverse_read_file) {
+        while(read_rf.read_read()) {
         //read_rf.read1->trim_qual_right("mean","lt",5,10,30);
-        if (trim_qual_right) {
-            read_rf.trim_qual_right("mean","lt",trim_qual_step,trim_qual_window,trim_qual_right_threshold);
+            if (trim_qual_right) {
+                read_rf.trim_qual_right("mean","lt",trim_qual_step,trim_qual_window,trim_qual_right_threshold);
+                }
+            if (trim_qual_left) {
+                read_rf.trim_qual_left("mean","lt",trim_qual_step,trim_qual_window,trim_qual_left_threshold);
             }
-        if (trim_qual_left) {
-            read_rf.trim_qual_left("mean","lt",trim_qual_step,trim_qual_window,trim_qual_left_threshold);
-        }
-        if (ns_max_n > -1 ) {read_rf.ns_max_n(ns_max_n);}
-        if (min_qual_mean)  {read_rf.min_qual_mean(min_qual_mean);}
-        if (min_qual_score) { read_rf.min_qual_score(min_qual_score);}
-        if (noiupac) {read_rf.noiupac();}
-        if (min_len) {read_rf.min_len(min_len);}
-        if (max_len) {read_rf.max_len(max_len);}
-        if (max_gc < 100) {read_rf.max_gc(max_gc);}
-        if (min_gc > 0) {read_rf.min_gc(min_gc);}
-        if (derep) {
-            read_rf.set_read_status(filter->contains(read_rf.read1->seq_seq),filter->contains(read_rf.read2->seq_seq));
-            filter->insert(read_rf.read1->seq_seq);
-            filter->insert(read_rf.read2->seq_seq);
-        }
+            if (ns_max_n > -1 ) {read_rf.ns_max_n(ns_max_n);}
+            if (min_qual_mean)  {read_rf.min_qual_mean(min_qual_mean);}
+            if (min_qual_score) { read_rf.min_qual_score(min_qual_score);}
+            if (noiupac) {read_rf.noiupac();}
+            if (min_len) {read_rf.min_len(min_len);}
+            if (max_len) {read_rf.max_len(max_len);}
+            if (max_gc < 100) {read_rf.max_gc(max_gc);}
+            if (min_gc > 0) {read_rf.min_gc(min_gc);}
+            if (derep) {
+                read_rf.set_read_status(filter->contains(read_rf.read1->seq_seq),filter->contains(read_rf.read2->seq_seq));
+                filter->insert(read_rf.read1->seq_seq);
+                filter->insert(read_rf.read2->seq_seq);
+            }
         
-        if (lc_entropy) {read_rf.entropy(entropy_threshold);}
-        if (lc_dust) {read_rf.dust(dust_threshold);}
+            if (lc_entropy) {read_rf.entropy(entropy_threshold);}
+            if (lc_dust) {read_rf.dust(dust_threshold);}
         
-        read_rf.print();
+            read_rf.print();
+        } 
+    } else {
+        while(read_f.read_read()) {
+            if (ns_max_n > -1 ) {read_f.ns_max_n(ns_max_n);}
+                read_f.print(out_format);
+        }
     }
-
-    inFile_f.close();  
-    inFile_r.close();  
+    
+    
+    inFile_f.close();
+    if (reverse_read_file){ inFile_r.close(); }  
     return 0;
 }
 
