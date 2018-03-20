40ffdad64d93910d22e98a49bed5ee6a83d450aa
Adrian Cantu <garbanyo@gmail.com>
2018-03-20 17:52:17 +0000

    more documentation

--- src/main.cpp
+++ src/main.cpp
@@ -505,6 +505,10 @@ Option:
     -v | -version
         Print version; ignore other arguments.
         
+    -threads 
+        Nuber of threads to use. Note that if more than one thread is used, output
+        sequences might not be in the same order as input sequences. (Default=1)
+    
     ***** INPUT OPTIONS *****
     
     -fastq <file>
@@ -513,11 +517,12 @@ Option:
     -fastq2 <file>
         Input file in FASTQ format for pair-end reads. Can also read a 
         compressed (.gz) file.
+        
     
     ***** OUTPUT OPTION *****
     
     -out_format <int>
-        Set output format. 0 FASTQ, 1 FASTA. [Default=0]
+        Set output format. 0 FASTQ, 1 FASTA. (Default=0)
         
     -out_name <string>
         For pair-end sequences, the output files are <string>_good_out_R1 and
@@ -529,6 +534,9 @@ Option:
     -rm_header
         Remove the header in the 3rd line of the fastq (+header -> +). This does
         not change the header in the 1st line (@header).
+        
+    -out_gz 
+        Write the output to a compressed file (WARNING this can be really SLOW)
         
     ***** FILTER OPTION ******
         
@@ -557,25 +565,51 @@ Option:
     -noiupac         
         Filter sequence with characters other than A, C, G, T or N.
 
-    -derep           
-         
+    -derep
+        Filter duplicated sequences. This only remove exact duplicates.
         
--lc_entropy      
--lc_dust         
--trim_qual_right 
--trim_qual_left  
--trim_qual_type  
--trim_qual_rule  
--trim_qual_window
--trim_qual_step  
-        
-       
--trim_tail_left  
--trim_tail_right 
--out_gz          
--threads         
-            
+    -lc_entropy=[float]
+        Filter sequences with entropy lower than [float]. [float] should be in
+        the 0-1 interval. (Default=0.5)
 
+    -lc_dust=[float]
+        Filter sequences with dust_score lower than [float]. [float] should be in
+        the 0-1 interval. (Default=0.5)
+        
+    ***** TRIM OPTIONS *****
+
+    -trim_tail_left <integer>
+        Trim poly-A/T tail with a minimum length of <integer> at the
+        5'-end.
+
+    -trim_tail_right <integer>
+        Trim poly-A/T tail with a minimum length of <integer> at the
+        3'-end.
+
+    -trim_qual_rule <string>
+        Rule to use to compare quality score to calculated value. Allowed
+        options are lt (less than), gt (greater than) and et (equal to).
+        [default: lt]
+
+    -trim_qual_left=[float]
+        Trim recursively from the 3'-end chunks of length -trim_qual_step if the
+        mean quality of the first -trim_qual_window bases is less than [float]. 
+        (Default=20)
+        
+    -trim_qual_right=[float]
+        Trim recursively from the 5'-end chunks of length -trim_qual_step if the
+        mean quality of the last -trim_qual_window bases is less than [float]. 
+        (Default=20)    
+
+    -trim_qual_window [int]
+        Size of the window used by trim_qual_left and trim_qual_right (Default=5)
+
+    -trim_qual_step [int]
+        Step size used by trim_qual_left and trim_qual_right (Default=2)
+    
+    -trim_qual_type <string>
+        Type of quality score calculation to use. Allowed options are min,
+        mean, max and sum. [default= min]
         )*";
     std::cout << help_msn << std::endl;
     
