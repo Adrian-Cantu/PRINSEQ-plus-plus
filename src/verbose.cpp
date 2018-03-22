e38d8344a86208ae10e92cf42f27ec198b662296
Adrian Cantu <garbanyo@gmail.com>
2018-03-22 16:57:44 +0000

    new files for verbose filter

---
+++ src/verbose.cpp
@@ -0,0 +1,24 @@
+#ifndef IOSTREAM
+#define IOSTREAM
+#include <iostream>
+#endif
+
+#ifndef REGEX
+#define REGEX
+#include <regex>
+#endif
+
+#ifndef MATH
+#define MATH
+#include <math.h>
+#endif
+
+#ifndef PTHREAD
+#define PTHREAD
+#include <pthread.h>
+#endif
+
+#include "reads.h"
+#include <unordered_map>
+#include <algorithm> 
+#include <numeric>
---
+++ src/verbose.h
@@ -0,0 +1,21 @@
+#ifndef IOSTREAM
+#define IOSTREAM
+#include <iostream>
+#endif
+
+#ifndef REGEX
+#define REGEX
+#include <regex>
+#endif
+
+#ifndef MATH
+#define MATH
+#include <math.h>
+#endif
+
+#ifndef PTHREAD
+#define PTHREAD
+#include <pthread.h>
+#endif
+
+using namespace std;
